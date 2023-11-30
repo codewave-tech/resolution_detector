5a25149eb1d39e95c178a387bc66ae92615d71b1
touheed1 <touheed@codewave.com>
2023-11-30 14:50:38 +0530

    added license

--- test/resolution_detector_method_channel_test.dart
+++ test/resolution_detector_method_channel_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:resolution_detector/resolution_detector_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:resolution_detector/resolution_detector_method_channel.dart';
 
-void main() {
-  MethodChannelResolutionDetector platform = MethodChannelResolutionDetector();
-  const MethodChannel channel = MethodChannel('resolution_detector');
+// void main() {
+//   // MethodChannelResolutionDetector platform = MethodChannelResolutionDetector();
+//   // const MethodChannel channel = MethodChannel('resolution_detector');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  // setUp(() {
-  //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
-  //     return '42';
-  //   });
-  // });
+//   // setUp(() {
+//   //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//   //     return '42';
+//   //   });
+//   // });
 
-  // tearDown(() {
-  //   channel.setMockMethodCallHandler(null);
-  // });
+//   // tearDown(() {
+//   //   channel.setMockMethodCallHandler(null);
+//   // });
 
-  // test('getMaxResolution', () async {
-  //   expect(await platform.getMaxResolution(), '42');
-  // });
-}
+//   // test('getMaxResolution', () async {
+//   //   expect(await platform.getMaxResolution(), '42');
+//   // });
+// }
